a4fdae5eac96bc45cd74cbec508ce53ea6a4ecae
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-12-24 23:10:10 +0900

    CIツールのセットアップ - セキュリティ (#2)
    * type-checkコマンドの追加
    
    * prettierのセットアップ
    
    * eslint連携
    
    * husky, lint-stagedのインストール
    
    * CI用のyaml作成
    
    * pnpm-auditの実行
    
    * ワークフローエラーの修正
    
    * depandabot.ymlの作成
    
    * semgrepのセットアップ
    
    * Secret Scanningのコメントアウト実装
    
    * Flake追加
    
    * OSV Scannerのジョブを追加
    
    * sarifアップロード
    
    * ワークフロー実行エラーの修正
    
    * sarifアップロードの無効化

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig: NextConfig = {
   basePath: BASE_PATH,
-  output: "export",
+  output: 'export',
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
-import nextConfig from "../../next.config";
-const BASE_PATH = nextConfig.basePath || "";
+import nextConfig from '../../next.config';
+const BASE_PATH = nextConfig.basePath || '';
 
 export default function Home() {
   return (
@@ -19,22 +19,22 @@ export default function Home() {
         <div className={styles.intro}>
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a starting point or more instructions? Head over to{' '}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               Templates
-            </a>{" "}
-            or the{" "}
+            </a>{' '}
+            or the{' '}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               Learning
-            </a>{" "}
+            </a>{' '}
             center.
           </p>
         </div>
